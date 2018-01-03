c8e72aeb9c5e90be0cd23d091b775a8ea7ea2ac3
Ishai Levi <levishai87@gmail.com>
2018-01-03 11:10:36 +0200

    network connector

---
+++ app/src/main/java/course/android/letgo_300432317_303062210/utils/ResStatus.java
@@ -0,0 +1,9 @@
+package course.android.letgo_300432317_303062210.utils;
+
+/**
+ * enum class that for the status of update resources process
+ */
+public enum ResStatus {
+    SUCCESS,
+    FAIL
+}
